5a40ac6e7315a1ef598f7debb663a9e1f351664e
MDSAMIULSAMI <mdsamiulislam2172@gmail.com>
2024-08-02 23:08:07 +0600

    New Problem Solved

--- A Set/677A.py
+++ A Set/677A.py
@@ -1,1 +1,11 @@
 friend, height = map(int, input().split())
+
+point = [int(item) for item in input().split()]
+
+counter = 0
+for i in range(friend):
+    if point[i] > height:
+        counter = counter + 2
+    else:
+        counter = counter + 1
+print(counter)
